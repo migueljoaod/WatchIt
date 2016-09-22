d6eeef31643d9e139e7fcfea5e9c96b8877f489a
miguel joao <migueljoaodias@gmail.com>
2016-09-22 20:29:22 -0300

    Adicionando toolbar e lista de series

--- app/src/main/java/com/example/joaosampaio/watchit/ListaSeriesActivity.java
+++ app/src/main/java/com/example/joaosampaio/watchit/ListaSeriesActivity.java
@@ -8,16 +8,20 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
 
 
 public class ListaSeriesActivity extends AppCompatActivity {
     private MaterialSearchView searchView;
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_listaserie, menu);
+        getMenuInflater().inflate(R.menu.menu_listaserie, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
         return true;
     }
 
@@ -37,6 +41,38 @@ public class ListaSeriesActivity extends AppCompatActivity {
         //Define o Toolbar como ActionBar
         //para permitir a compatibilidade
         setSupportActionBar(toolbar);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        searchView = (MaterialSearchView) findViewById(R.id.search_view);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(ListaSeriesActivity.this, "Pesquisar", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //Do some magic
+                return false;
+            }
+        });
+
+        searchView.setOnSearchViewListener(new MaterialSearchView.SearchViewListener() {
+            @Override
+            public void onSearchViewShown() {
+                //Do some magic
+            }
+
+            @Override
+            public void onSearchViewClosed() {
+                //Do some magic
+            }
+        });
 
     }
 }
